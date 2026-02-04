0f9ce95d5644bf3d65f07719c821098f8f1d3f95
MaksymMartseniuk <marcenukm995@gmail.com>
2026-02-04 22:28:36 +0200

    Refactor follow_cylinder.cpp by removing unused includes and organizing namespace declarations

--- experiments/follow_cylinder.cpp
+++ experiments/follow_cylinder.cpp
@@ -1,13 +1,19 @@
 #define RL_TOOLS_BACKEND_ENABLE_OPENBLAS
 #include <rl_tools/operations/cpu_mux.h>
-#include <rl_tools/nn/optimizers/adam/instance/operations_generic.h>
-#include <rl_tools/nn/optimizers/adam/operations_generic.h>
-#include <rl_tools/nn/layers/standardize/operations_generic.h>
-#include <rl_tools/nn/layers/dense/operations_cpu_openblas.h>
 #include <rl_tools/nn_models/operations_cpu.h>
+#include <rl_tools/rl/environments/l2f/multirotor.h>
+#include <rl_tools/rl/environments/l2f/operations_cpu.h>
 
 
 namespace rlt= rl_tools;
+using DEVICE=rlt::devices::DEVICE_FACTORY<>;
+using RNG= DEVICE::SPEC::RANDOM::ENGINE<>;
+using T=float;
+using TYPE_POLICY = rlt::numeric_types::Policy<T>;
+using TI = typename DEVICE::index_t;
+
+
+
 
 int main(int argc, char** argv)
 {
