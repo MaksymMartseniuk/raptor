024cea939a5ea2e813f526640f5d4405a96a36f2
MaksymMartseniuk <marcenukm995@gmail.com>
2026-02-04 21:42:38 +0200

    Add follow cylinder experiment for RL training

---
+++ experiments/follow_cylinder.cpp
@@ -0,0 +1,10 @@
+#define RL_TOOLS_BACKEND_ENABLE_OPENBLAS
+#include <rl_tools/operations/cpu_mux.h>
+#include <rl_tools/nn/optimizers/adam/instance/operations_generic.h>
+#include <rl_tools/nn/optimizers/adam/operations_generic.h>
+#include <rl_tools/nn/layers/standardize/operations_generic.h>
+#include <rl_tools/nn/layers/dense/operations_cpu_openblas.h>
+#include <rl_tools/nn_models/operations_cpu.h>
+
+namespace rlt=rl_tools;
+
